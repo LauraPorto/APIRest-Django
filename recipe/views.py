9fe0fd62992026df99a136c0095a02ff10d06e7e
Laura <lauraporto596@gmail.com>
2021-07-06 11:15:43 +0200

    serializers and views for tags

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+from core.models import Tag 
+
+from recipe import serializers
 # Create your views here.
+
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    #manejar los tags en la base de datos
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Tag.objects.all()
+    serializers_class = serializers.TagSerializer
