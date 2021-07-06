83cbfe973a6c22f0e2128aef1fb5d9c450ac9b9b
Laura <lauraporto596@gmail.com>
2021-07-06 12:08:43 +0200

    views and serializer, test OK for ingredient

--- recipe/views.py
+++ recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag 
+from core.models import Tag, Ingredient 
 
 from recipe import serializers
 # Create your views here.
@@ -13,6 +13,22 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateMo
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializer_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        #retornar objetos para el usuario autenticado
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        #crear un nuevo ingredientes
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    #manejar los tags en la base de datos
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializer_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         #retornar objetos para el usuario autenticado
